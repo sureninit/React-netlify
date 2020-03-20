96c03ce7ae68f440f1a9cd4a96fb2cffae2e12c0
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-03-19 20:48:11 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Nav from './Nav';
+import Shop from './Shop';
+import {BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <switch>
+      
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      
+      <Route path="/shop" component={Shop} />
+      </switch>
+    
     </div>
+    </Router>
   );
 }
-
+const Home = () =>(
+  <div>
+    <h1>Home Pages</h1>
+  </div>
+)
 export default App;
---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+const Shop = () => (
+<div className="App">
+  
+  <h1>shop page</h1>
+  
+  </div>
+);
+
+
+export default Shop;
